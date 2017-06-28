703635eef40eade51e6434f0b9dcf099acb92e87
Daniel Martí <mvdan@mvdan.cc>
2017-06-28 09:41:49 +0100

    load defaults as interface{}

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -12,12 +15,12 @@ import (
 
 var (
 	tmpl *template.Template
+
+	conf, def map[string]interface{}
 )
 
 func main() {
-	var err error
-	tmpl, err = template.ParseFiles("index.html")
-	if err != nil {
+	if err := load(); err != nil {
 		log.Fatal(err)
 	}
 	r := chi.NewRouter()
@@ -30,13 +33,40 @@ func main() {
 	http.ListenAndServe(":8080", r)
 }
 
+func load() error {
+	var err error
+	tmpl, err = template.ParseFiles("index.html")
+	if err != nil {
+		return err
+	}
+	if conf, err = parseFile(filepath.Join("default", "conf.json")); err != nil {
+		return err
+	}
+	if def, err = parseFile(filepath.Join("default", "def.json")); err != nil {
+		return err
+	}
+	return nil
+}
+
+func parseFile(path string) (map[string]interface{}, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	var m map[string]interface{}
+	err = json.NewDecoder(f).Decode(&m)
+	return m, err
+}
+
 type snippet struct {
 	Conf, Def string
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	jsonConf, _ := json.MarshalIndent(conf, "", "\t")
+	jsonDef, _ := json.MarshalIndent(def, "", "\t")
 	tmpl.Lookup("index.html").Execute(w, snippet{
-		Conf: "here is conf",
-		Def:  "here is def",
+		Conf: string(jsonConf),
+		Def:  string(jsonDef),
 	})
 }
